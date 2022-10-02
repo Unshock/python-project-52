db1707d01f136ed6e471b3b1ce9a6fb89bb81bb5
Nedashkovsky <Nedashkovsky@ymail.com>
2022-10-02 15:20:53 +0300

    some adds for heroku

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
+import os
 
 
 def index(request):
+    times = int(os.environ.get('TIMES', 3))
     return render(request, 'index.html', context={
-        "who": "World",
+        "who": "World"*times,
     })
 
 
