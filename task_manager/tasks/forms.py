859704bed9d5233456f7a81a36432036bbd37d2b
Nedashkovsky <Nedashkovsky@ymail.com>
2022-10-29 17:27:29 +0300

    fixet choise executor of task

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,27 +1,30 @@
 from django.forms import ModelForm
 from django import forms
-
+from django.utils.translation import gettext_lazy as _
 from task_manager.labels.models import Label
 from task_manager.tasks.models import Task
 from task_manager.statuses.models import Status
 from task_manager.user.models import User
 
+class FullNameChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, obj):
+        return obj.full_name
 
 class TaskForm(ModelForm):
     name = forms.CharField(
-        label='Имя', widget=forms.TextInput(
+        label=_('Name'), widget=forms.TextInput(
             attrs={"class": "form-control"})
     )
     description = forms.CharField(
-        label='Описание',
+        label=_('Description'),
         widget=forms.Textarea(
             attrs={"class": "form-control",
                    "rows": "10",
                    "cols": "40",
                    })
     )
-    executor = forms.ModelChoiceField(
-        label='Исполнитель',
+    executor = FullNameChoiceField(
+        label=_('Executor'),
         queryset=User.objects.all(),
         widget=forms.Select(
             attrs={"class": "form-control"}
@@ -30,7 +33,7 @@ class TaskForm(ModelForm):
         empty_label='No executor'
     )
     labels = forms.ModelMultipleChoiceField(
-        label='Метки',
+        label=_('Labels'),
         queryset=Label.objects.all(),
         widget=forms.SelectMultiple(
             attrs={"class": "form-control"}
@@ -38,7 +41,7 @@ class TaskForm(ModelForm):
         required=False,
     )
     status = forms.ModelChoiceField(
-        label='Статус',
+        label=_('Status'),
         queryset=Status.objects.all(),
         widget=forms.Select(
             attrs={"class": "form-control"}
