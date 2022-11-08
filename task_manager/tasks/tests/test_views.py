819db185703178ad4f390cedae22266eac33d50a
Nedashkovsky <Nedashkovsky@ymail.com>
2022-11-08 21:51:21 +0300

    fix linter

--- task_manager/tasks/tests/test_views.py
+++ task_manager/tasks/tests/test_views.py
@@ -1,7 +1,6 @@
 from http import HTTPStatus
 from django.urls import reverse, resolve
 from django.utils.translation import gettext_lazy as _
-from task_manager.statuses.models import Status
 from .setting import SettingsTasks
 from .. import views
 from ..models import Task
